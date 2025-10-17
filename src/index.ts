387c5ba7b806f62b6c7e239db1c2f9e4820bd207
James Benner <hello@jamesbenner.com>
2025-10-17 17:16:43 -0400

    Prune some package props before copying

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import esbuild from 'esbuild';
 import type { BuildOptions } from 'esbuild';
 import { $ } from 'execa';
+import isPlainObject from 'is-plain-obj';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import sortPackageJson from 'sort-package-json';
@@ -30,16 +31,38 @@ async function readPackageJson() {
 }
 
 async function writePackageJson(type: 'commonjs' | 'module'): Promise<void> {
+  const basePath = type === 'commonjs' ? CJS_PATH : ESM_PATH;
   const packageJson = await readPackageJson();
+  const ensureDotForwardSlashPrefix = (value: string) =>
+    value.startsWith('./') ? value : `./${value}`;
+
+  if (
+    isPlainObject(packageJson.exports) &&
+    isPlainObject(packageJson.exports.import) &&
+    isPlainObject(packageJson.exports.require)
+  ) {
+    // The `exports` property now has incorrect paths, so we fix this according to the module type.
+    packageJson.types = path.posix.relative(basePath, packageJson.exports.import.types);
+    packageJson.exports = ensureDotForwardSlashPrefix(
+      type === 'commonjs'
+        ? path.posix.relative(basePath, packageJson.exports.require.default)
+        : path.posix.relative(basePath, packageJson.exports.import.default)
+    );
+  } else {
+    console.warn(
+      'The `exports` property in the `package.json` is either undefined, not an object,' +
+      ' or not of the expected object structure.'
+    );
+  }
 
   if (type === 'module') {
     // If we're writing the ESM package.json, then the bin file paths are wrong. I don't think it
     // really matters, but we fix it here just in case.
     if (typeof packageJson.bin === 'string') {
-      packageJson.bin = path.posix.relative(ESM_PATH, packageJson.bin);
+      packageJson.bin = path.posix.relative(basePath, packageJson.bin);
     } else {
       Object.entries(packageJson.bin ?? {}).forEach(([key, value]) => {
-        packageJson.bin[key] = path.posix.relative(ESM_PATH, value as string);
+        packageJson.bin[key] = path.posix.relative(basePath, value as string);
       });
     }
   } else {
@@ -48,7 +71,15 @@ async function writePackageJson(type: 'commonjs' | 'module'): Promise<void> {
     delete packageJson.bin;
   }
 
-  const filepath = [type === 'commonjs' ? CJS_PATH : ESM_PATH, 'package.json'].join('/');
+  // Some package properties are invalid, problematic, or complex to fix in this new context. So we
+  // just delete them outright.
+  delete packageJson.dependencies;
+  delete packageJson.devDependencies;
+  delete packageJson.files;
+  delete packageJson.peerDependencies;
+  delete packageJson.scripts;
+
+  const filepath = `${basePath}/package.json`;
   const data = JSON.stringify({ ...packageJson, type }, null, 2);
 
   await fs.writeFile(filepath, sortPackageJson(data));
