bf25671aa0504692f9e8b08b91dce80db47734d3
James Benner <hello@jamesbenner.com>
2025-10-17 16:30:20 -0400

    Normalize paths and copy package.json contents

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,19 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+[Unreleased]
+------------
+
+### Changed
+
+- Normalized the app to use POSIX paths. Some things like `package.bin` can apparently break if
+  using Windows paths.
+- The contents of the root `package.json` are now copied into the respective
+  `dist/{cjs,esm}/package.json` files before being potentially modified. The mostly empty
+  `package.json` files which were being created before were causing issues with apps which utilized
+  the `meow` package (_e.g._, the `version` property was missing so the `--version` flag was
+  broken).
+
 [0.1.1] - 2025-10-17
 --------------------
 
@@ -20,5 +33,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Initial release.
 
+[Unreleased]: https://github.com/jbenner-radham/node-duo-build/compare/v0.1.1...HEAD
 [0.1.1]: https://github.com/jbenner-radham/node-duo-build/compare/v0.1.0...v0.1.1
 [0.1.0]: https://github.com/jbenner-radham/node-duo-build/releases/tag/v0.1.0
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import type { BuildOptions } from 'esbuild';
 import { $ } from 'execa';
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import sortPackageJson from 'sort-package-json';
 
 const COMMON_ESBUILD_OPTIONS = {
   bundle: true,
-  entryPoints: [path.join('src', 'index.ts')],
+  entryPoints: ['src/index.ts'],
   minify: true,
   sourcemap: true
 };
-const CJS_PATH = path.join('dist', 'cjs');
-const ESM_PATH = path.join('dist', 'esm');
+const CJS_PATH = 'dist/cjs';
+const ESM_PATH = 'dist/esm';
 
 async function isFile(filepath: string): Promise<boolean> {
   try {
@@ -21,25 +22,49 @@ async function isFile(filepath: string): Promise<boolean> {
   }
 }
 
-async function writePackageJsonFile(type: 'commonjs' | 'module'): Promise<void> {
-  const packageJson = JSON.stringify({ type }, null, 2);
-  const filepath = path.join(type === 'commonjs' ? CJS_PATH : ESM_PATH, 'package.json');
+async function readPackageJson() {
+  const buffer = await fs.readFile('package.json');
+  const source = buffer.toString();
 
-  await fs.writeFile(filepath, packageJson);
+  return JSON.parse(source);
+}
+
+async function writePackageJson(type: 'commonjs' | 'module'): Promise<void> {
+  const packageJson = await readPackageJson();
+
+  if (type === 'module') {
+    // If we're writing the ESM package.json, then the bin file paths are wrong. I don't think it
+    // really matters, but we fix it here just in case.
+    if (typeof packageJson.bin === 'string') {
+      packageJson.bin = path.posix.relative(ESM_PATH, packageJson.bin);
+    } else {
+      Object.entries(packageJson.bin ?? {}).forEach(([key, value]) => {
+        packageJson.bin[key] = path.posix.relative(ESM_PATH, value as string);
+      });
+    }
+  } else {
+    // If we're writing the CJS package.json, then the bin file paths are invalid. Because we always
+    // run our CLI apps via ESM.
+    delete packageJson.bin;
+  }
+
+  const filepath = [type === 'commonjs' ? CJS_PATH : ESM_PATH, 'package.json'].join('/');
+  const data = JSON.stringify({ ...packageJson, type }, null, 2);
+
+  await fs.writeFile(filepath, sortPackageJson(data));
 }
 
 async function buildCjs(options: BuildOptions = {}): Promise<void> {
   const defaultOptions = { ...COMMON_ESBUILD_OPTIONS, format: 'cjs' as const, outdir: CJS_PATH };
 
   await esbuild.build({ ...defaultOptions, ...options });
-  await writePackageJsonFile('commonjs');
+  await writePackageJson('commonjs');
 }
 
 async function buildEsm(options: BuildOptions = {}): Promise<void> {
-  const cliScriptPath = path.join('src', 'cli.ts');
-  const isCliApp = await isFile(cliScriptPath);
+  const isCliApp = await isFile('src/cli.ts');
   const entryPoints = isCliApp
-    ? [...COMMON_ESBUILD_OPTIONS.entryPoints, cliScriptPath]
+    ? [...COMMON_ESBUILD_OPTIONS.entryPoints, 'src/cli.ts']
     : COMMON_ESBUILD_OPTIONS.entryPoints;
   const defaultOptions = {
     ...COMMON_ESBUILD_OPTIONS,
@@ -49,7 +74,7 @@ async function buildEsm(options: BuildOptions = {}): Promise<void> {
   };
 
   await esbuild.build({ ...defaultOptions, ...options });
-  await writePackageJsonFile('module');
+  await writePackageJson('module');
 }
 
 async function buildTypes(): Promise<void> {
